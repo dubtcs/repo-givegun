dddf57f601e268104caeb824b65fe865b09af4dd
dubtcs <75275524+dubtcs@users.noreply.github.com>
2026-07-16 17:48:56 -0400

    Init

---
+++ givegun.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace givegun;
+
+[BepInPlugin("plspls.givegun", "givegun", "1.0")]
+public class GiveGun : BaseUnityPlugin
+{
+    internal static GiveGun Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger => Instance._logger;
+    private ManualLogSource _logger => base.Logger;
+    internal Harmony? Harmony { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        
+        // Prevent the plugin from being deleted
+        this.gameObject.transform.parent = null;
+        this.gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+        Patch();
+
+        Logger.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");
+    }
+
+    internal void Patch()
+    {
+        Harmony ??= new Harmony(Info.Metadata.GUID);
+        Harmony.PatchAll();
+    }
+
+    internal void Unpatch()
+    {
+        Harmony?.UnpatchSelf();
+    }
+
+}
---
+++ givergun_giver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using SingularityGroup.HotReload;
+using UnityEngine;
+
+namespace givegun;
+
+[HarmonyPatch(typeof(RoundDirector))]
+static class GiveGun_Giver
+{
+    private const string ITEM_NAME = "Item Gun Handgun";
+
+    [HarmonyPrefix, HarmonyPatch(nameof(RoundDirector.StartRound))]
+    private static void Start_Prefix()
+    {
+        if (!SemiFunc.IsMainMenu() && SemiFunc.IsMasterClientOrSingleplayer())
+        {
+            Dictionary<string, int> purchased = StatsManager.instance.itemsPurchased;
+            int player_count = Math.Max(1, SemiFunc.PlayerGetList().Count);
+            int item_count = purchased.GetValueOrDefault(ITEM_NAME, 0);
+            purchased[ITEM_NAME] = Math.Max(player_count, item_count);
+            if(player_count < item_count)
+            {
+                int dif = player_count - item_count;
+                GiveGun.Logger.LogInfo($"Adding {dif} guns.");
+            }
+        }
+    }
+}
