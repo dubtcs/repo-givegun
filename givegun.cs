ead9ee202fc53c281a2ebdb8012c450749eeebae
dubtcs <75275524+dubtcs@users.noreply.github.com>
2026-07-19 22:43:17 -0400

    Only applies on run start, now uses RunManager patch

--- givegun.cs
+++ givegun.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 namespace givegun;
 
-[BepInPlugin("plspls.givegun", "givegun", "1.0")]
+[BepInPlugin("plspls.Loadout", "Loadout", "1.2.2")]
 public class GiveGun : BaseUnityPlugin
 {
     internal static GiveGun Instance { get; private set; } = null!;
@@ -16,6 +16,7 @@ public class GiveGun : BaseUnityPlugin
 
     public static ConfigEntry<string> item_list;
     public static ConfigEntry<int> item_count;
+    public static ConfigEntry<bool> mod_enabled;
 
     private void Awake()
     {
@@ -24,19 +25,17 @@ public class GiveGun : BaseUnityPlugin
         this.gameObject.hideFlags = HideFlags.HideAndDontSave;
         item_list = Config.Bind("Items", "ItemList", "Item Gun Handgun", "A list of items you want to start with separated by a semicolon. Use a hash to denote quantity. Items without quantity will be spawned for each player.");
         item_count = Config.Bind("Items", "ItemMaxOverride", 4, new ConfigDescription("The max amount of each item instance allowed to be spawned.", new AcceptableValueRange<int>(0, int.MaxValue)));
+        mod_enabled = Config.Bind("General", "Enabled", true, "Enable or disable the mod.");
         Patch();
         Logger.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");
     }
-
     internal void Patch()
     {
         Harmony ??= new Harmony(Info.Metadata.GUID);
         Harmony.PatchAll();
     }
-
     internal void Unpatch()
     {
         Harmony?.UnpatchSelf();
     }
-
 }
--- givergun_giver.cs
+++ givergun_giver.cs
@@ -1,23 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Xml.Linq;
-using BepInEx.Configuration;
-using ExitGames.Client.Photon.StructWrapping;
 using HarmonyLib;
-using Photon.Realtime;
 using SingularityGroup.HotReload;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace givegun;
 
-[HarmonyPatch(typeof(RoundDirector))]
+[HarmonyPatch(typeof(RunManager))]
 static class GiveGun_Giver
 {
-    // private const string ITEM_NAME = "Item Gun Handgun";
+    private static void SetItemMax(string item_name)
+    {
+        if (GiveGun.item_count.Value == 0)
+            return;
+        Item[] items = Resources.FindObjectsOfTypeAll<Item>();
+        foreach (Item i in items)
+        {
+            if (i.name == item_name)
+            {
+                i.maxAmount = Math.Max(i.maxAmount, GiveGun.item_count.Value);
+            }
+        }
+    }
 
-    // Sets the purchase records of the item to the number passed into as argument
     private static void PurchaseItems(string item_name, int count)
     {
         Dictionary<string, int> purchased = StatsManager.instance.itemsPurchased;
@@ -25,39 +31,27 @@ static class GiveGun_Giver
         purchased[item_name] = Math.Max(count, item_count);
     }
 
-    private static void SetItemMax(string item_name)
+    [HarmonyPostfix, HarmonyPatch(nameof(RunManager.ChangeLevel))]
+    private static void Patch()
     {
-        if(GiveGun.item_count.Value == 0)
-            return;
-        Item[] items = Resources.FindObjectsOfTypeAll<Item>();
-        foreach(Item i in items)
+        if (GiveGun.mod_enabled.Value && SemiFunc.IsMasterClientOrSingleplayer())
         {
-            if(i.name == item_name)
+            if (SemiFunc.RunIsLevel() && RunManager.instance.loadLevel == 0)
             {
-                i.maxAmount = Math.Max(i.maxAmount, GiveGun.item_count.Value);
+                GiveGun.Logger.LogMessage($"First round, supplying items.");
+                List<PlayerAvatar> players = SemiFunc.PlayerGetList();
+                int player_count = Math.Max(1, players.Count); // Use max and 1 here to cover offline SP bc idk if GetPlayerList() works there
+                List<string> items = GiveGun.item_list.Value.Split(';').ToList();
+                foreach (string s in items)
+                {
+                    List<string> i = s.Split('#').ToList();
+                    string item_name = i[0].TrimStart().TrimEnd();
+                    int count = (i.Count > 1 && int.TryParse(i[1], out count)) ? count : player_count;
+                    GiveGun.Logger.LogMessage($"Giving {count} {item_name}.");
+                    SetItemMax(item_name);
+                    PurchaseItems(item_name, count);
+                }
             }
         }
     }
-    
-    [HarmonyPostfix, HarmonyPatch(nameof(RoundDirector.StartRound))]
-    private static void Patcher()
-    {
-        if (SemiFunc.RunIsLevel() && SemiFunc.IsMasterClientOrSingleplayer())
-        {
-            List<PlayerAvatar> players = SemiFunc.PlayerGetList();
-            // Max with 1 accounts for singleplayer
-            int player_count = Math.Max(1, players.Count);
-            List<string> items = GiveGun.item_list.Value.Split(';').ToList();
-            foreach(string s in items)
-            {
-                List<string> i = s.Split('#').ToList();
-                string item_name = i[0].TrimStart().TrimEnd();
-                int count = (i.Count > 1 && int.TryParse(i[1], out count)) ? count : player_count;
-                GiveGun.Logger.LogMessage($"Giving {count} {item_name}.");
-                SetItemMax(item_name);
-                PurchaseItems(item_name, count);
-            }
-        }
-    }
-
 }
