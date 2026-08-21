738e1ff62835ad37cd57421ab18d51675a70bf43
dubtcs <75275524+dubtcs@users.noreply.github.com>
2026-08-20 22:07:35 -0400

    Clearer enable/disable UI state

--- loadout_ui.cs
+++ loadout_ui.cs
@@ -22,41 +22,55 @@ static class LoadoutUI
 
     static private void CreateItemEntries(REPOPopupPage menu)
     {
-        menu.AddElementToScrollView(view => MenuAPI.CreateREPOSpacer(view, size: new Vector2(0, 20)).rectTransform);
-        menu.AddElementToScrollView(view =>
-            {
-                REPOSlider slider = MenuAPI.CreateREPOSlider("Preset", string.Empty, nv =>
+        if (Loadout.IsEnabledAndMaster())
+        {
+            menu.AddElementToScrollView(view => MenuAPI.CreateREPOSpacer(view, size: new Vector2(0, 20)).rectTransform);
+            menu.AddElementToScrollView(view =>
                 {
-                    selected_loadout = nv;
-                }, view, default, 1, Loadout.MAX_LOADOUTS, selected_loadout);
-                return slider.rectTransform;
-            });
-        menu.AddElementToScrollView(view =>
-        {
-            REPOLabel label = MenuAPI.CreateREPOLabel("Set an item quantity to -1 to spawn one for each player.", view, Vector2.zero);
-            label.labelTMP.fontSize = 12;
-            return label.rectTransform;
-        });
-        menu.AddElementToScrollView(view => MenuAPI.CreateREPOSpacer(view, size: new Vector2(0, 20)).rectTransform);
-        sliders.Clear();
-        foreach (string s in StatsManager.instance.itemDictionary.Keys)
-        {
+                    REPOSlider slider = MenuAPI.CreateREPOSlider("Preset", string.Empty, nv =>
+                    {
+                        selected_loadout = nv;
+                    }, view, default, 1, Loadout.MAX_LOADOUTS, selected_loadout);
+                    return slider.rectTransform;
+                });
             menu.AddElementToScrollView(view =>
             {
-                REPOSlider slider = MenuAPI.CreateREPOSlider(s, string.Empty, nv =>
-                {
-                    int value = Loadout.loadout[s];
-                    if (int.TryParse(nv.ToString(), out value))
-                    {
-                        Loadout.loadout[s] = value;
-                    }
-                }, view, default, -1, 20, Loadout.loadout[s]);
-                sliders[s] = slider;
-                return slider.rectTransform;
+                REPOLabel label = MenuAPI.CreateREPOLabel("Set an item quantity to -1 to spawn one for each player.", view, Vector2.zero);
+                label.labelTMP.fontSize = 12;
+                return label.rectTransform;
             });
-            menu.AddElementToScrollView(scrollView => MenuAPI.CreateREPOSpacer(scrollView, size: new Vector2(0, 10)).rectTransform);
+            menu.AddElementToScrollView(view => MenuAPI.CreateREPOSpacer(view, size: new Vector2(0, 20)).rectTransform);
+            sliders.Clear();
+            foreach (string s in StatsManager.instance.itemDictionary.Keys)
+            {
+                menu.AddElementToScrollView(view =>
+                {
+                    REPOSlider slider = MenuAPI.CreateREPOSlider(s, string.Empty, nv =>
+                    {
+                        int value = Loadout.loadout[s];
+                        if (int.TryParse(nv.ToString(), out value))
+                        {
+                            Loadout.loadout[s] = value;
+                        }
+                    }, view, default, -1, 20, Loadout.loadout[s]);
+                    sliders[s] = slider;
+                    return slider.rectTransform;
+                });
+                menu.AddElementToScrollView(scrollView => MenuAPI.CreateREPOSpacer(scrollView, size: new Vector2(0, 10)).rectTransform);
+            }
+            menu.AddElementToScrollView(scrollView => MenuAPI.CreateREPOSpacer(scrollView, size: new Vector2(0, 20)).rectTransform);
         }
-        menu.AddElementToScrollView(scrollView => MenuAPI.CreateREPOSpacer(scrollView, size: new Vector2(0, 20)).rectTransform);
+        else
+        {
+            menu.AddElementToScrollView(view => MenuAPI.CreateREPOSpacer(view, size: new Vector2(0, 20)).rectTransform);
+            menu.AddElementToScrollView(view =>
+                        {
+                            REPOLabel label = MenuAPI.CreateREPOLabel("Mod disabled, change in settings.", view, Vector2.zero);
+                            label.labelTMP.fontSize = 12;
+                            return label.rectTransform;
+                        });
+            menu.AddElementToScrollView(view => MenuAPI.CreateREPOSpacer(view, size: new Vector2(0, 20)).rectTransform);
+        }
     }
 
     static private void Loadout_OpenUI()
@@ -70,25 +84,28 @@ static class LoadoutUI
             return true;
         };
         CreateItemEntries(menu);
-        menu.AddElement(parent =>
+        if (Loadout.IsEnabledAndMaster())
         {
-            MenuAPI.CreateREPOButton("SAVE PRESET", () =>
+            menu.AddElement(parent =>
             {
-                Loadout.loadouts[selected_loadout - 1] = Loadout.GetLoadoutString();
-                Loadout.SavePresets();
-            }, parent, new Vector2(370f, 18f));
-        });
-        menu.AddElement(parent =>
-        {
-            MenuAPI.CreateREPOButton("LOAD PRESET", () =>
-            {
-                Loadout.SetLoadoutFromString(Loadout.loadouts[selected_loadout - 1], true);
-                foreach (string s in StatsManager.instance.itemDictionary.Keys)
+                MenuAPI.CreateREPOButton("SAVE PRESET", () =>
                 {
-                    sliders[s].value = Loadout.loadout[s];
-                }
-            }, parent, new Vector2(585f, 18f));
-        });
+                    Loadout.loadouts[selected_loadout - 1] = Loadout.GetLoadoutString();
+                    Loadout.SavePresets();
+                }, parent, new Vector2(370f, 18f));
+            });
+            menu.AddElement(parent =>
+            {
+                MenuAPI.CreateREPOButton("LOAD PRESET", () =>
+                {
+                    Loadout.SetLoadoutFromString(Loadout.loadouts[selected_loadout - 1], true);
+                    foreach (string s in StatsManager.instance.itemDictionary.Keys)
+                    {
+                        sliders[s].value = Loadout.loadout[s];
+                    }
+                }, parent, new Vector2(585f, 18f));
+            });
+        }
     }
 
     static public void CreateMenus()
